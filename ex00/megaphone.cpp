27a9eeca198978515888dd4e6e40159e0d10d6dd
kjroy93 <kmarrero1993@gmail.com>
2026-06-10 20:08:12 +0200

    Makefile modified

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: kjroydev <kjroydev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 16:11:15 by kjroydev          #+#    #+#             */
-/*   Updated: 2026/05/24 21:34:49 by kjroydev         ###   ########.fr       */
+/*   Updated: 2026/06/10 19:56:45 by kjroydev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,9 @@ int	main(int argc, char *argv[])
 				str[j] = (char)std::toupper(str[j]);
 				j++;
 			}
-			std::cout << str << " ";
+			std::cout << str << std::endl;
 			i++;
 		}
 	}
-	std::cout << std::endl;
 	return (0);
 }
